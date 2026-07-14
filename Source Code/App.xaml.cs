e87f3260ecf93c24aa93416ce4a7cdb4299ca162
Nick Brown <nickbrown1990@hotmail.com>
2026-07-14 16:01:43 -0500

    Add files via upload

---
+++ Source Code/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using D424.Data;
+using D424.Views;
+
+namespace D424;
+
+public partial class App : Application
+{
+    private readonly AppDatabase _database;
+
+    public App(AppDatabase database)
+    {
+        InitializeComponent();
+        _database = database;
+        //_database.SeedDataAsync().Wait();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new NavigationPage(new TermListPage(_database)));
+    }
+}
---
+++ Source Code/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace D424
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
